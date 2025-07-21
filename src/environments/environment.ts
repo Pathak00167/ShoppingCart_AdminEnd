c26059ae52c80915d0abe190ce52230134f89851
patha <patha@PATHAK>
2025-07-21 22:40:35 +0530

    New Changees

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://192.168.117.217:5112/api',// 192.168.8.217
-     imageUrl: 'http://192.168.117.217:5112/',
+     apiUrl: 'http://10.131.246.217:5112/api',//  http://192.168.171.217:5112/
+     imageUrl: 'http://10.131.246.217:5112/',//10.131.246.217
   };
   
